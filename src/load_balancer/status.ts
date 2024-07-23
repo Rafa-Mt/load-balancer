f0358d300086e40fa80224c9b6948f7c27e96dbb
JoseCarrilloURU <jose.30739823@uru.edu>
2024-07-23 11:22:12 -0400

    ou yeah

--- src/load_balancer/status.ts
+++ src/load_balancer/status.ts
@@ -2,51 +2,89 @@ import { getStatus } from "./dispatcherClient";
 import { getHealth } from "./healthClient";
 import type { Dir, BalancerClient } from ".";
 
-export const calcStatus = async (dirs:Dir[], clients:BalancerClient[]) => {
-    const results: any = {};
+interface ResultValue {
+	effectivity: number,
+	activeRequests: number,
+	freeMemory: number,
+	cpuUsage: number
+}
+type Result = Record<string, ResultValue>;
 
-	clients.forEach(async client => {
+interface ArrayResult { 
+	effectivity: number,
+	activeRequests: number,
+	freeMemory: number,
+	cpuUsage: number,
+	addr: string
+}
+
+export const calcStatus = async (dirs:Dir[], clients:BalancerClient[]) => {
+	const results: Result = {};
+	const arrayResults: ArrayResult[] = []
+
+	for (const client of clients) {
+
 		const {health, dispatcher} = client
 
 		const gottenHealth: any = await getHealth(health.client.client, health.client.service, health.addr)
+		console.log(gottenHealth, "health")
 		const gottenStatus: any = await getStatus(dispatcher.client.client, dispatcher.client.service, dispatcher.addr)
-			if (!(gottenHealth instanceof Error) && !(gottenStatus instanceof Error))
-				Object.assign(results, {[dispatcher.addr]: {
-					effectivity: gottenStatus.effectivity,
-					activeRequests: gottenStatus.activeRequests,
-					freeMemory: gottenHealth.freeMemory,
-					cpuUsage: gottenHealth.cpuUsage
-				}})
-	})
+		console.log(gottenStatus, "status")
+		if (!(gottenHealth instanceof Error) && !(gottenStatus instanceof Error)) {
+			
+			const res = {
+				effectivity: gottenStatus[dispatcher.addr].effectivity,
+				activeRequests: gottenStatus[dispatcher.addr].activeRequests,
+				freeMemory: gottenHealth[health.addr].freeMemory,
+				cpuUsage: gottenHealth[health.addr].cpuUsage
+			}
+			const arrayRes = {
+				effectivity: gottenStatus[dispatcher.addr].effectivity,
+				activeRequests: gottenStatus[dispatcher.addr].activeRequests,
+				freeMemory: gottenHealth[health.addr].freeMemory,
+				cpuUsage: gottenHealth[health.addr].cpuUsage,
+				addr: dispatcher.addr
+			}
+			
+			Object.assign(results, { [dispatcher.addr]: res })
+			arrayResults.push(arrayRes)
+		}
+	}
 
-
-	// for (let dir of dirs) {
-	// 	const {health, dispatcher} = clients[dir];
-	// 	console.log(clients[dir])
-	// 	return
-	// 	const gottenHealth:any = await getHealth(health.client, health.service, dir);
-	// 	const gottenStatus:any = await getStatus(dispatcher.client, dispatcher.service, dir);
 
 	const table = Object.entries(results);
-	
-	const effectivity = table.map((entry:any) => Object.fromEntries([entry[0], entry[1].effectivity]))
+	console.log("table:", table)
+
+
+	const effectivity = arrayResults.map((entry) =>
+	{
+		return { "addr": entry.addr, "effectivity": entry.effectivity }
+	})
 	const maxEffectivity = Math.max(...effectivity.map((item) => item.effectivity));
-	const effectivityWinner = effectivity.filter((item) => item.effectivity === maxEffectivity)
+	const effectivityWinner = effectivity.find((item) => item.effectivity === maxEffectivity)
 
-	const activeRequests = table.map((entry:any) => Object.fromEntries([entry[0], entry[1].activeRequests]))
+	const activeRequests = arrayResults.map((entry) =>
+	{
+		return { "addr": entry.addr, "activeRequests": entry.activeRequests }
+	})
 	const minActive = Math.min(...activeRequests.map((item) => item.activeRequests))
-	const requestWinner = activeRequests.filter((item) => item.activeRequests === minActive)
+	const requestWinner = activeRequests.filter((item) => item.activeRequests === minActive)[0]
 
-	const freeMemory = table.map((entry:any) => Object.fromEntries([entry[0], entry[1].freeMemory]))
+	const freeMemory = arrayResults.map((entry) =>
+	{
+		return { "addr": entry.addr, "freeMemory": entry.freeMemory < 0 ? -entry.freeMemory : entry.freeMemory }
+	})
 	const maxMemory = Math.max(...freeMemory.map((item) => item.freeMemory))
-	const memoryWinner = freeMemory.filter((item) => item.freeMemory === maxMemory)
+	const memoryWinner = freeMemory.find((item) => item.freeMemory === maxMemory)
 
-	const cpuUsage = table.map((entry:any) => Object.fromEntries([entry[0], entry[1].cpuUsage]))
+	const cpuUsage = arrayResults.map((entry) =>
+	{
+		return { "addr": entry.addr, "cpuUsage": entry.cpuUsage }
+	})
 	const minCpu = Math.min(...cpuUsage.map((item) => item.cpuUsage))
-	const cpuWinner = cpuUsage.filter((item) => item.cpuUsage === minCpu)
+	const cpuWinner = cpuUsage.find((item) => item.cpuUsage === minCpu)
 
 	const finalTable = [ effectivityWinner, requestWinner, memoryWinner, cpuWinner ];
-	const keys = finalTable.map((column) => column.keys())
 
-	console.log("results:", finalTable)
+	return finalTable
 }
